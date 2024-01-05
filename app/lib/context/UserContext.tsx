02358da9f7f9863f0b252b6aa440c6906fa9b6cd
Daniil <zelenko.danil@gmail.com>
2024-01-05 21:46:49 +0100

    UserContext provide obj that can update user state

--- app/lib/context/UserContext.tsx
+++ app/lib/context/UserContext.tsx
@@ -3,29 +3,47 @@
 import { createContext, useEffect, useState } from "react";
 import { getProfile } from "../request/userProfile";
 
-type User = {
+type Profile = {
   id: number;
   name: string;
   isAuthorized: boolean
 }
 
-export const UserContext = createContext<User | null>(null);
+export type UserState = {
+  profile: Profile | null,
+  updateUser: (data: any) => void;
+}
+
+export const UserContext = createContext<UserState>({
+  profile: null,
+  updateUser: () => {
+    throw new Error('User context is not set');
+  }
+});
 
 export const UserProvider = ({ children }: {
   children: React.ReactNode;
 }) => {
-  const [ user, setUser ] = useState<User | null>(null);
+
+  const [ profile, setProfile ] = useState<Profile | null>(null);
 
   useEffect(() => {
     // TODO: Potential XSS vulnerability, change it in future
     const accessToken = localStorage.getItem('accessToken');
     if (accessToken) {
-      getProfile(accessToken).then((user) => setUser(user))
+      getProfile(accessToken).then((profile) => setProfile(profile))
     }
   }, []);
 
+  const stateUser: UserState = {
+    profile,
+    updateUser: (profile: Profile) => {
+      setProfile(profile);
+    }
+  }
+
   return (
-    <UserContext.Provider value={user}>
+    <UserContext.Provider value={stateUser}>
       {children}
     </UserContext.Provider>
   );
