0d40844cd3c0fd93f210b98ff31b1cde24dbcf5e
Daniil <zelenko.danil@gmail.com>
2024-01-08 15:11:29 +0100

    remove import

--- app/ui/index.ts
+++ app/ui/index.ts
@@ -1,5 +1,4 @@
-import { Login } from './Login/Login';
 import { Navbar } from './Navbar/Navbar';
 import { Notifications } from './Notification/Notification';
 
-export { Login, Navbar, Notifications };
+export { Navbar, Notifications };
