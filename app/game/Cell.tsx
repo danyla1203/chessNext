aa84346a223277a80e5653b032620e8a37201116
Daniil <zelenko.danil@gmail.com>
2024-01-10 21:41:26 +0100

    styling dots

--- app/game/Cell.tsx
+++ app/game/Cell.tsx
@@ -12,7 +12,7 @@ export function CellItem({
 }: any) {
   const socket = useWebSocket();
 
-  let classbase = 'figure size-20';
+  let classbase = 'figure size-20 flex justify-center items-center';
 
   if (highlighted) classbase += ` bg-slate-400`;
   else classbase += ` ${color}`;
@@ -20,7 +20,7 @@ export function CellItem({
   if (figure) classbase += ` ${figure.replace(/\d/, '')} ${side}`;
   return (
     <div onClick={() => cellClick(coord)} className={classbase}>
-      <span className="board__row__cell-dot">{dotted ? '*' : ''}</span>
+      {dotted ? <span className="w-5 h-5 rounded-full bg-slate-400" /> : null}
     </div>
   );
 }
