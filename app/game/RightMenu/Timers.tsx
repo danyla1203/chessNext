414cb6e6a9dac5dbb9d46c1b2b267086f3538bfe
Daniil <zelenko.danil@gmail.com>
2024-01-12 21:56:27 +0100

    beauty timer

--- app/game/RightMenu/Timers.tsx
+++ app/game/RightMenu/Timers.tsx
@@ -1,3 +1,15 @@
 export function Timer({ time }: { time: number }) {
-  return <div>{time}</div>;
+  const minutes = Math.floor(time / (1000 * 60));
+  const seconds = Math.round((time - minutes * 1000 * 60) / 1000);
+
+  let beautySeconds;
+  if (seconds === 0) beautySeconds = '00';
+  else beautySeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  const beautyMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  return (
+    <div>
+      {beautyMinutes}:{beautySeconds}
+    </div>
+  );
 }
