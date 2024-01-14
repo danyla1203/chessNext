0347ef00f224959ad24571a95023f07782107c74
Daniil <zelenko.danil@gmail.com>
2024-01-14 03:23:25 +0100

    striked figure style

--- app/game/RightMenu/index.tsx
+++ app/game/RightMenu/index.tsx
@@ -2,21 +2,38 @@ import { useConfigContext } from '../context';
 import { useStrikedFigures } from '../context/Striked';
 import { TimersController } from './Timers.controller';
 
+function Striked({ figure, side }: { figure: string; side: 'w' | 'b' }) {
+  return (
+    <div
+      className={`striked__figure ${figure.replace(/\d/, '')} ${side}`}
+      key={`${figure}-b`}
+    />
+  );
+}
+
 export function RightMenu() {
   const strikedFigures = useStrikedFigures();
   const { side } = useConfigContext();
+
+  const strikedW = strikedFigures.w.map((f) => (
+    <Striked side="w" key={f} figure={f} />
+  ));
+  const strikeB = strikedFigures.b.map((f) => (
+    <Striked side="b" key={f} figure={f} />
+  ));
+
   return (
     <div>
       {side === 'w' ? (
-        <div>{strikedFigures.w}</div>
+        <div className="flex">{strikedW}</div>
       ) : (
-        <div>{strikedFigures.b}</div>
+        <div className="flex">{strikeB}</div>
       )}
       <TimersController />
       {side === 'w' ? (
-        <div>{strikedFigures.b}</div>
+        <div className="flex">{strikeB}</div>
       ) : (
-        <div>{strikedFigures.w}</div>
+        <div className="flex">{strikedW}</div>
       )}
     </div>
   );
