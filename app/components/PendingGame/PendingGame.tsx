d076e44249cd2cb27e67d6103dc7da4b86142724
Daniil <zelenko.danil@gmail.com>
2024-01-16 22:31:43 +0100

    pending game styles

--- app/components/PendingGame/PendingGame.tsx
+++ app/components/PendingGame/PendingGame.tsx
@@ -1,19 +1,22 @@
 'use client';
 
 import { useGameList } from '@/context/GameListContext';
+import { Button, Spinner } from '@nextui-org/react';
 
 export function PendingGame() {
   const { pendingGame, rejoin } = useGameList();
 
   if (!pendingGame) return null;
 
+  const startContent = <Spinner color="warning" size="sm" className="mr-2" />;
+
   return (
-    <div
-      className="cursor-pointer flex items-center justify-center shadow-md p-2 px-4 bg-yellow-300"
+    <Button
+      color="primary"
+      startContent={startContent}
       onClick={() => rejoin(pendingGame)}
     >
-      <div className="mr-2 animate-spin rounded-full h-4 w-4 border-t-2 border-amber-600 border-r-2 border-b-2 border-gray-300"></div>
       Pending Game
-    </div>
+    </Button>
   );
 }
