c582b839576d56a0b77cf42815556d080258c07e
Daniil <zelenko.danil@gmail.com>
2024-01-17 15:42:55 +0100

    chat styles

--- app/game/Chat.tsx
+++ app/game/Chat.tsx
@@ -1,24 +1,30 @@
 import { useState } from 'react';
+import { Input, Card, CardBody, Button } from '@nextui-org/react';
 import { useChat } from './context/Chat';
 import { Message } from './types';
 
-function Input({ addMessage }: { addMessage: (text: string) => void }) {
+function InputBox({ addMessage }: { addMessage: (text: string) => void }) {
   const [val, setVal] = useState('');
 
+  const overridedClick = () => {
+    addMessage(val);
+    setVal('');
+  };
+
   return (
-    <div className="flex flex-col items-center">
-      <input
-        className="w-full h-8 w-full box-border border-2 border-blue-500 outline-none p-2"
-        onChange={(e) => setVal(e.target.value)}
+    <>
+      <Input
+        radius="none"
         value={val}
+        onValueChange={setVal}
+        type="email"
+        placeholder="Message"
+        size="sm"
       />
-      <button
-        className="h-8 w-full box-border bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
-        onClick={() => addMessage(val)}
-      >
-        Send
-      </button>
-    </div>
+      <Button radius="none" onClick={overridedClick}>
+        Send message
+      </Button>
+    </>
   );
 }
 
@@ -32,12 +38,12 @@ function MessageItem({ message }: { message: Message }) {
   return (
     <div className="flex justify-between w-full mb-3">
       <div className="items-center w-5/6">
-        <h5 className="mr-3 text-lg font-semibold">{message.author.name}</h5>
-        <p className="text-gray-700 break-all">{message.text}</p>
-
-        {/* Adding the whitespace-normal class to allow text wrapping */}
+        <h5 className="text-lg font-bold text-zinc-400">
+          {message.author.name}
+        </h5>
+        <p className="test-sm">{message.text}</p>
       </div>
-      <h4 className="text-sm text-gray-500">{beautyTime}</h4>
+      <h4 className="text-sm font-bold text-zinc-400">{beautyTime}</h4>
     </div>
   );
 }
@@ -46,13 +52,15 @@ export function Chat() {
   const { messages, addMessage } = useChat();
 
   return (
-    <div className="flex flex-col h-full w-76 min-w-52 mr-7 bg-white shadow-md p-3">
-      <div className="flex flex-col h-[calc(100%-3.5rem)] w-full">
-        {messages.map((m) => (
-          <MessageItem key={m.id} message={m} />
-        ))}
-      </div>
-      <Input addMessage={addMessage} />
-    </div>
+    <Card className="h-full max-w-80 w-80 min-w-5 rounded-r-none">
+      <CardBody>
+        <div className="flex flex-col h-[calc(100%-3.5rem)] w-full">
+          {messages.map((m) => (
+            <MessageItem key={m.id} message={m} />
+          ))}
+        </div>
+      </CardBody>
+      <InputBox addMessage={addMessage} />
+    </Card>
   );
 }
