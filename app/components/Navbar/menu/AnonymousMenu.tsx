ca2c26f93249891755130e0998931a30eec83cf6
Daniil <zelenko.danil@gmail.com>
2024-01-24 19:58:53 +0100

    login and profile page for anonymous user

--- app/components/Navbar/menu/AnonymousMenu.tsx
+++ app/components/Navbar/menu/AnonymousMenu.tsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import { DropdownUi } from '@/ui/Dropdown';
 
 export function AnonymousMenu() {
-  const contents = [{ href: '/authorization', name: 'Login' }];
+  const contents = [
+    { href: '/user', name: 'Profile' },
+    { href: '/authorization', name: 'Login' },
+  ];
   return <DropdownUi label="Anonymous" contents={contents} />;
 }
