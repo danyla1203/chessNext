5f10b0b54e2f44c534994065c9ba02decc43d989
Daniil <zelenko.danil@gmail.com>
2024-01-25 16:56:16 +0100

    fetch games

--- app/lib/request/userProfile.ts
+++ app/lib/request/userProfile.ts
@@ -1,13 +1,21 @@
 export const getProfile = async (accessToken: string) => {
   const host = process.env.NEXT_PUBLIC_API_HOST as string;
   try {
-    return (
+    const user = await (
       await fetch(`${host}/user`, {
         headers: {
           Authorization: 'Bearer ' + accessToken,
         },
       })
     ).json();
+    const games = await (
+      await fetch(`${host}/user/games`, {
+        headers: {
+          Authorization: 'Bearer ' + accessToken,
+        },
+      })
+    ).json();
+    return { ...user, ...games, isAuthorized: true };
   } catch (e) {
     console.error(e);
     return e;
