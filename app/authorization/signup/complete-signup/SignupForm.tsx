3b2fba3be4c52074900610a26e06220a7bfc2108
Daniil <zelenko.danil@gmail.com>
2024-01-27 23:27:29 +0100

    complete registration styles

--- app/authorization/signup/complete-signup/SignupForm.tsx
+++ app/authorization/signup/complete-signup/SignupForm.tsx
@@ -5,6 +5,15 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import { createUser } from '@/requests';
 import { useUserState } from '@/context/UserContext';
 import { useNotification } from '@/context/NotificationContext';
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Chip,
+  Input,
+} from '@nextui-org/react';
 
 export function SignupForm() {
   const [userName, setName] = useState('');
@@ -29,54 +38,32 @@ export function SignupForm() {
   };
 
   return (
-    <div className="flex flex-col items-start justify-start pt-10 pr-10 pb-10 pl-10 bg-white shadow-2xl rounded-xl relative z-10">
-      <p className="w-full text-4xl font-medium text-center leading-snug font-serif">
-        Sign up for an account
-      </p>
-      <div className="relative mt-3">
-        <p className="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600 absolute">
-          {email}
-        </p>
-      </div>
-      <div className="w-full mt-6 mr-0 mb-0 ml-0 relative space-y-8">
-        <div className="relative">
-          <p className="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600 absolute">
-            Username
-          </p>
-          <input
-            placeholder="John"
-            type="text"
+    <Card className="z-10">
+      <CardHeader>
+        <h1 className="w-full text-4xl">Sign up for an account</h1>
+      </CardHeader>
+      <CardBody>
+        <Chip>{email}</Chip>
+        <div className="w-full mt-6 mr-0 mb-0 ml-0 relative space-y-4">
+          <Input
+            placeholder="Username"
+            size="sm"
             value={userName}
             onChange={(e) => setName(e.target.value)}
-            className="border placeholder-gray-400 focus:outline-none
-                  focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
-                  border-gray-300 rounded-md"
           />
-        </div>
-        <div className="relative">
-          <p className="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600 absolute">
-            Password
-          </p>
-          <input
+          <Input
             placeholder="Password"
-            type="password"
+            size="sm"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="border placeholder-gray-400 focus:outline-none
-                  focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
-                  border-gray-300 rounded-md"
           />
         </div>
-        <div className="relative">
-          <button
-            onClick={signUp}
-            className="w-full inline-block pt-4 pr-5 pb-4 pl-5 text-xl font-medium text-center text-white bg-indigo-500
-                  rounded-lg transition duration-200 hover:bg-indigo-600 ease"
-          >
-            Submit
-          </button>
-        </div>
-      </div>
-    </div>
+      </CardBody>
+      <CardFooter>
+        <Button onClick={signUp} fullWidth color="primary" variant="ghost">
+          Submit
+        </Button>
+      </CardFooter>
+    </Card>
   );
 }
