d137691951a9bef22885fd3b8126cb7caadf975a
Daniil <zelenko.danil@gmail.com>
2024-08-16 16:44:03 +0200

    profile created date

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -9,6 +9,7 @@ import {
   CardHeader,
   Chip,
 } from '@nextui-org/react';
+import moment from 'moment';
 import { GamesTable } from './GamesTable';
 import { Loader } from '@/components';
 import { UserBalance } from './UserBalance';
@@ -26,20 +27,27 @@ export default function UserProfile() {
   return (
     <div className="flex justify-center mt-5 p-9">
       <Card className="mr-4 h-fit">
-        <CardHeader className="pb-1">
+        <CardHeader className="pb-1 flex-col items-start">
           <h1>{profile.name}</h1>
+          {authorized ? (
+            <h5 className="text-xs">
+              Become player {moment(profile.createdAt).fromNow()}
+            </h5>
+          ) : null}
         </CardHeader>
         <CardBody className="pb-0 pt-2">
           {/* Wait for backend */}
-          <h3 className="text-xs font-semibold">
-            Authorize first to see Rating
-          </h3>
+          {!authorized ? (
+            <h3 className="text-xs font-semibold">
+              Authorize first to see Rating
+            </h3>
+          ) : null}
           <div className="flex flex-col xl:flex-row space-y-2 xl:space-y-0 mt-4 mb-4 h-full">
             <Chip className="mr-1">Wins: {profile.wins}</Chip>
             <Chip className="mr-1">Looses: {profile.looses}</Chip>
             <Chip>Draws: {profile.draws}</Chip>
           </div>
-          {profile.isAuthorized ? <UserBalance profile={profile} /> : null}
+          {authorized ? <UserBalance profile={profile} /> : null}
         </CardBody>
         <CardFooter className="pt-1">
           <div className="w-full flex flex-col place-items-stretch justify-between">
@@ -54,7 +62,7 @@ export default function UserProfile() {
       </Card>
       <div className="w-3/4 2xl:flex">
         <GamesTable profile={profile} />
-        {profile.isAuthorized ? <InvoiceHistory profile={profile} /> : null}
+        {authorized ? <InvoiceHistory profile={profile} /> : null}
       </div>
     </div>
   );
